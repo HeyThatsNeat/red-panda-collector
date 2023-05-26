805a4dd8b0fc2b3c70fb3cfd7eca26c9b6d1d14e
Hunter Eckhoff <Phantazm777@verizon.net>
2023-05-26 17:22:30 -0400

    add About and All Red Pandas links and on the red-pandas there are a list of them

--- main_app/views.py
+++ main_app/views.py
@@ -4,8 +4,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+class RedPanda:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+red_pandas = [
+  RedPanda('Lolo', 'tabby', 'Kinda rude.', 3),
+  RedPanda('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  RedPanda('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  RedPanda('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+# Add new view
+
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return HttpResponse('<h1>Hello</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About the RedPandaCollector</h1>')
+  return render(request, 'about.html')
+
+def red_panda_index(request):
+  return render(request, 'red-pandas/index.html', { 'red_pandas': red_pandas })
