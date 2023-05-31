34f64330558b38f1cb2857ae66234aa8bb749921
Hunter Eckhoff <Phantazm777@verizon.net>
2023-05-30 20:02:54 -0400

    update nav to be better practice

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('red-pandas/', views.red_panda_index, name='red_panda_index'),
+  path('red-pandas/', views.red_panda_index, name='red-panda-index'),
   path('red-pandas/<int:red_panda_id>/', views.red_panda_detail, name='red-panda-detail'),
 ]
